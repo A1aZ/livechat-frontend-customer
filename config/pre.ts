289e3ad0885bb0c53117d80614572f59e3b2ce76
Alan <tozhufeng@gmail.com>
2025-09-21 03:10:44 +0800

    feat: 在 h5 配置中新增 WebpackChain 插件配置，禁用 React Fast Refresh 相关代码注入并处理相关变量

--- config/pre.ts
+++ config/pre.ts
@@ -7,5 +7,28 @@ export default {
   mini: {},
   h5: {
     publicPath: '/customer/',
+    /**
+     * WebpackChain 插件配置
+     * @docs https://github.com/neutrinojs/webpack-chain
+     */
+    webpackChain(chain) {
+      // 禁用 React Fast Refresh 相关的代码注入
+      chain.plugin('react-refresh').tap(args => {
+        if (args.length > 0) {
+          args[0].overlay = false;
+        }
+        return args;
+      });
+
+      // 处理 Fast Refresh 相关变量
+      chain.plugin('define').tap(args => {
+        if (args.length > 0) {
+          args[0]['process.env.NODE_ENV'] = JSON.stringify('production');
+          args[0]['$RefreshReg$'] = '(() => {})';
+          args[0]['$RefreshSig$'] = '(() => (type) => type)';
+        }
+        return args;
+      });
+    }
   },
 } satisfies UserConfigExport<'webpack5'>
